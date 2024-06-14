3025bc1c8879ab9b9d626d00bc3312e7ff7d1f6f
Gabriel Afonso Pantoja <bielbj@gmail.com>
2024-06-14 16:58:17 -0300

    segunda questão
    adicionando o codigo da 2 questão

--- automato.py
+++ automato.py
@@ -8,7 +8,6 @@ class Automato(object):
 
 
     def mostraAutomato(self):
-        print('teste')
         print('estados: ',self.e)
         print('transicoes: ',self.t)
 
@@ -32,3 +31,34 @@ class Automato(object):
             print('PALAVRA ACEITA')
         else:
             print('PALAVRA RECUSADA')    
+
+    def questao2(self, texto):
+        print (texto)
+        match = False
+        inicioCount = 0
+        finalCount = 0
+        inicio = 0
+        final = 0
+        for i in texto: #percorrendo a cadeia
+            #print('lendo a letra', i)
+            match = False
+            for j in self.t: #para cada letra da cadeia, verifica se existe uma transição naquele estado
+                #print('comparando a letra: ',i,' no estado:',self.atual,'com a transição: ',j,)
+                if (i == j[1]) & (self.atual == j[0]): #verificando a existencia da transição
+                    #print(self. atual, ' -> ',i,' -> ',j[2])
+                    if (self.atual == 'q0') & (j[2] == 'q1'):
+                        #print('entrou aqui')
+                        inicio = inicioCount + 1
+                    elif (self.atual == 'q11') & (j[2] == 'q12'):
+                        final = finalCount - 1
+                        print('casamento nas posições', inicio,' ', final)
+                    self.atual = j[2]
+                    #print('atual agora ', self.atual)
+                    match = True
+                    break
+            if not match:
+                #print('nao foi')
+                self.atual = 'q0'
+
+            inicioCount += 1
+            finalCount += 1
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-string = 'testando a string'
 from automato import Automato
 
 '''
@@ -46,28 +45,63 @@ transicoes = [
     ['q3','c','q3']
 
 ]
-
 inicial = 'q0'
 finais = ['q1','q3']
 
-'''
-estados = ['q0','q1','q2','q3']
+
+======questão 2
+
+estados = ['q0','q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11']
 transicoes = [
-    ['q0','a','q1'],
-    ['q0','b','q2'],
-    ['q1','a','q1'],
-    ['q1','b','q2'],
-    ['q1','c','q3'],
-    ['q2','a','q3'],
-    ['q2','b','q2'],
-    ['q3','c','q3']
+    ['q0',' ','q1'],
+    ['q1','c','q2'],
+    ['q2','o','q3'],
+    ['q3','m','q4'],
+    ['q4','p','q5'],
+    ['q5','u','q6'],
+    ['q6','t','q7'],
+    ['q7','a','q8'],
+    ['q8','d','q9'],
+    ['q9','o','q10'],
+    ['q10','r','q11'],
+    ['q11',' ','q12']
+
+    inicial = 'q1'
+    finais = ['q11']
+
+    texto = 'O computador é uma máquina capaz de variados tipos de tratamento automático de
+informações ou processamento de dados. Entende-se por computador um sistema físico que
+realiza algum tipo de computação. Assumiu-se que os computadores pessoais e laptops são
+ícones da era da informação. O primeiro computador eletromecânico foi construído por
+Konrad Zuse (1910–1995). Atualmente, um microcomputador é também chamado
+computador pessoal ou ainda computador doméstico.'
 
 ]
 
+
+'''
+estados = ['q0','q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11']
+transicoes = [
+    ['q0',' ','q1'],
+    ['q1','c','q2'],
+    ['q2','o','q3'],
+    ['q3','m','q4'],
+    ['q4','p','q5'],
+    ['q5','u','q6'],
+    ['q6','t','q7'],
+    ['q7','a','q8'],
+    ['q8','d','q9'],
+    ['q9','o','q10'],
+    ['q10','r','q11'],
+    ['q11',' ','q12']
+]
 inicial = 'q0'
-finais = ['q1','q3']
+finais = ['q11']
+
+texto = "O computador é uma máquina capaz de variados tipos de tratamento automático de informações ou processamento de dados. Entende-se por computador um sistema físico que realiza algum tipo de computação. Assumiu-se que os computadores pessoais e laptops são ícones da era da informação. O primeiro computador eletromecânico foi construído por Konrad Zuse (1910–1995). Atualmente, um microcomputador é também chamado computador pessoal ou ainda computador doméstico." 
+#texto = 'o computador é uma maquina computador '
 aut = Automato(estados,transicoes,inicial,finais)
 
 #aut.mostraAutomato()
 
-aut.processaString('aba')
+aut.questao2(texto)
